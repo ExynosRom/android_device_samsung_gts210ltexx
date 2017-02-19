7d02fa036e252043603fac17faa22d6701454064
Alexander Gemsa <alexander.gemsa@gmail.com>
2017-02-19 20:31:16 +0100

    AEX Changes
    Change-Id: I65330722914f6e88bdce060b6d368e929c95592a

--- aosp_gts210ltexx.mk
+++ aosp_gts210ltexx.mk
@@ -5,11 +5,13 @@ $(call inherit-product, device/samsung/gts2-common/device-common.mk)
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common aosp stuff.
+$(call inherit-product, vendor/aosp/common.mk)
+
+
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := lineage_gts210ltexx
+PRODUCT_NAME := aosp_gts210ltexx
 PRODUCT_DEVICE := gts210ltexx
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-T815
