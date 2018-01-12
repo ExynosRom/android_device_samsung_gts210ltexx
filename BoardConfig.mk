d1b099f486cb37cfb77a2af309594e2ac06bbd5f
bonuzzz <bonuzzz@gmail.com>
2018-01-12 23:27:29 +0300

    Public init

---
+++ BoardConfig.mk
@@ -0,0 +1,20 @@
+LOCAL_PATH := device/samsung/gts210ltexx
+
+# Include path
+TARGET_SPECIFIC_HEADER_PATH := $(LOCAL_PATH)/include
+
+# Kernel
+TARGET_KERNEL_CONFIG := lineage_exynos5433_gts210ltexx_defconfig
+
+# RIL
+BOARD_MODEM_TYPE := ss333
+BOARD_PROVIDES_LIBRIL := true
+
+# RIL.java overwrite
+BOARD_RIL_CLASS := ../../../$(LOCAL_PATH)/ril
+
+# Recovery
+TARGET_OTA_ASSERT_DEVICE := gts210lte,gts210ltexx
+
+# Inherit common board flags
+include device/samsung/gts2-common/BoardConfigCommon.mk
---
+++ device.mk
@@ -0,0 +1,35 @@
+LOCAL_PATH := device/samsung/gts210ltexx
+
+# Ramdisk
+PRODUCT_PACKAGES += \
+	ueventd.universal5433.rc \
+    init.baseband.rc
+
+# Audio
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/audio/mixer_paths_0.xml:system/etc/mixer_paths_0.xml
+
+# Bluetooth
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/bluetooth/bt_vendor.conf:system/etc/bluetooth/bt_vendor.conf
+
+# Radio
+PRODUCT_PACKAGES += \
+    libxml2 \
+    libprotobuf-cpp-full
+
+PRODUCT_PACKAGES += \
+    libsecril-client \
+    libsecril-client-sap \
+    modemloader
+
+PRODUCT_PACKAGES += \
+    SamsungServiceMode
+
+# Permissions
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
+    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/vendor/etc/permissions/android.hardware.telephony.gsm.xml
+
+# call the proprietary setup
+$(call inherit-product, vendor/samsung/gts210ltexx/gts28ltexx-vendor.mk)
